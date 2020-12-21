402e1c93ea605572943e2ca2f619e35f3dde7cab
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-21 11:10:17 -0600

    added questions and selections to the app through functionality - have not allowed onClick yet to highlight the textview

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 import kotlin.math.log
 
 class QuizQuestionsActivity : AppCompatActivity() {
@@ -13,5 +14,20 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val questionsList = Constants.getQuestions()
         // Logcat --> verbose --> Question to see how large the questionList ArrayList is
         Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progress_bar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progress_bar.max
+
+        tv_question.text = question!!.question
+
+        iv_image.setImageResource(question.image)
+
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
     }
 }
